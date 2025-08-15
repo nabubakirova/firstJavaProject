aac8fda41a952168b1526757961441584e252296
nabubakirova <nagimka2512@gmail.com>
2025-08-15 12:52:27 -0700

    create a new class for teaching multithreading apllications

---
+++ src/new_topic/Multithreading.java
@@ -0,0 +1,9 @@
+package new_topic;
+
+public class Multithreading {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("here we will talk about multithreading applications");
+        }
+    }
+}
