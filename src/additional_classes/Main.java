06e6d4a15e748b16d563fcd341703ad1b1cc9982
nabubakirova <nagimka2512@gmail.com>
2025-08-14 19:42:10 -0700

    first commit

---
+++ src/additional_classes/Main.java
@@ -0,0 +1,10 @@
+package additional_classes;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("hello guys");
+            
+        }
+    }
+}
